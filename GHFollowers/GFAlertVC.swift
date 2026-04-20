0ea78de53eb5e90cd8beefd5b19730fef454ab01
Sergei Philippov <quadratte@gmail.com>
2026-04-20 22:49:20 +0300

    Add basic alertControl

---
+++ GHFollowers/GFAlertVC.swift
@@ -0,0 +1,35 @@
+
+import UIKit
+
+class GFAlertVC: UIViewController {
+    
+    let containerView = UIView()
+    let titleLabel = GFTitleLabel(textAlignment: .center, fontSize: 20)
+    let messageLabel = GFBodyLabel(textAlignment: .center)
+    let actionButton = GFButton(backgroundColor: .systemPink, title: "Ok")
+    
+    var alertTitle: String
+    var message: String
+    var buttonTitle: String
+    
+    init(alertTitle: String, message: String, buttonTitle: String) {
+        self.alertTitle = alertTitle
+        self.message = message
+        self.buttonTitle = buttonTitle
+        super.init(nibName: nil, bundle: nil)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.75)
+        view.addSubview(containerView)
+    }
+    
+    
+    
+}
