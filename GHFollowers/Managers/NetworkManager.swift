103911a679a53870dac4770a819fa1ed5d7725e7
Sergei Philippov <quadratte@gmail.com>
2026-04-23 23:31:54 +0300

    Added basic networking

---
+++ GHFollowers/Managers/NetworkManager.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+class NetworkManager {
+    
+    static let shared = NetworkManager()
+    let baseURL = "https://api.github.com/users"
+    
+    private init() { }
+    
+    func getFollowers(for username: String, page: Int, completed: @escaping([Follower]?, String?) -> Void ) {
+        
+        let endpoint = baseURL + ":\(username)/followers?per_page=100"
+        
+    }
+}
