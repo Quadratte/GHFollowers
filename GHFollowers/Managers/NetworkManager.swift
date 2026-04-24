f37593165b62ff7b8544f76369228acfed044140
Sergei Philippov <quadratte@gmail.com>
2026-04-24 23:12:37 +0300

    Singletone

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -8,8 +8,8 @@ class NetworkManager {
     private init() { }
     
     func getFollowers(for username: String, page: Int, completed: @escaping([Follower]?, String?) -> Void ) {
-        
         let endpoint = baseURL + ":\(username)/followers?per_page=100"
         
+        guard let url = URL(string: endpoint_, relativeTo: <#T##URL?#>)
     }
 }
