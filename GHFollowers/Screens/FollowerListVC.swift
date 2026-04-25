593c1a06859e2909655918624fa583d73d4a1818
Sergei Philippov <quadratte@gmail.com>
2026-04-26 00:06:04 +0300

    Refactor network layer

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -10,22 +10,19 @@ class FollowerListVC: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        NetworkManager.shared.getFollowers(for: userName, page: 1) { followers, errorMessage in
-            guard let followers = followers else {
-                self.presentGFAlertOnMainThred(title: "Bad stuff happend", message: errorMessage!, buttonTitle: "Ok")
-                return
+        NetworkManager.shared.getFollowers(for: userName, page: 1) { result in
+            
+            
+            switch result {
+            case .success(let followers):
+                print(followers)
+            case .failure(let error):                self.presentGFAlertOnMainThred(title: "Bad stuff happend", message: error.rawValue, buttonTitle: "Ok")
             }
-            print(followers)
-            print(errorMessage)
         }
-        
-        
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
-    
 }
