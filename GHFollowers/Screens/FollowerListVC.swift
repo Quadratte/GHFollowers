b3088f9798da1ae81a0da6eb37d0ce945fd18423
Sergei Philippov <quadratte@gmail.com>
2026-04-25 19:44:26 +0300

    Completed network manager

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -9,6 +9,18 @@ class FollowerListVC: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        NetworkManager.shared.getFollowers(for: userName, page: 1) { followers, errorMessage in
+            guard let followers = followers else {
+                self.presentGFAlertOnMainThred(title: "Bad stuff happend", message: errorMessage!, buttonTitle: "Ok")
+                return
+            }
+            print(followers)
+            print(errorMessage)
+        }
+        
+        
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
