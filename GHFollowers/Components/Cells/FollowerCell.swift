9fa59270ff13403bfabc89e64de759d44667cf73
Sergei Philippov <quadratte@gmail.com>
2026-04-26 23:48:22 +0300

    Added Custom ReusableCell

---
+++ GHFollowers/Components/Cells/FollowerCell.swift
@@ -0,0 +1,10 @@
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    static let reuseID = "FollowerCell"
+    
+    let avatarImageView = UIImageView()
+    let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16 )
+}
+
---
+++ GHFollowers/Components/GFAvatarImageView.swift
@@ -0,0 +1,14 @@
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
